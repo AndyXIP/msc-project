0e116abaf5dc164e20a4c1e4e453756e425572e0
AndyXIP <andy.x.p@outlook.com>
2025-08-15 11:00:33 +0100

    update save to json file

--- web-scraping/utils/save_data.py
+++ web-scraping/utils/save_data.py
@@ -2,8 +2,15 @@ import os
 import json
 
 def save_to_json(data, fname):
-    directory = "../data/raw/"
+    # Get directory of the current script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # Build path relative to this script
+    directory = os.path.join(script_dir, "..", "data", "raw")
     os.makedirs(directory, exist_ok=True)  # Ensure the directory exists
+
     filename = os.path.join(directory, fname)
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+
+    print(f"Saved {len(data)} items to {filename}")
